1912b82d0a59ed895812a76eecba3284a1e8f29b
shokouhSkr <shokouhskr@gmail.com>
2024-02-06 14:01:45 +0330

    add clerk authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <button className="btn btn-secondary">home</button>;
-}
+import Link from "next/link";
+
+const HomePage = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-6xl font-bold text-primary">GPTMuse</h1>
+
+          <p className="py-6 text-lg leading-loose">
+            GPTMuse: Your AI language companion. Powered by OpenAI, it enhances your conversations,
+            content creation, and more!
+          </p>
+
+          <Link href="/chat" className="btn btn-secondary ">
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
