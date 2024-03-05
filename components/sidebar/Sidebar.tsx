aa2bd5daf86394e1b60b5c562cda5ced1c0b9434
shokouhSkr <shokouhskr@gmail.com>
2024-03-05 23:47:10 +0330

    remove clerk

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
     <div className="min-h-full px-4 py-8 bg-primary border-r border-neutral w-72 grid grid-rows-[auto,1fr,auto] relative">
       <SidebarHeader />
       <NavLinks />
-      <MemberProfile />
+      {/* <MemberProfile /> */}
 
       <div className="absolute -bottom-[7px] -left-[25px] text-[15rem] text-gray-200 -z-10">
         <CgTrees />
